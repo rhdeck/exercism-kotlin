7c3ecd7dfc4f369a3e27e0c70fb794b96dd4e07a
Ray Deck <ray@raydeck.com>
2018-09-19 13:21:35 -0400

    Greening blank lines from cipher

--- simple-cipher/bin/cipher.kt
+++ simple-cipher/bin/cipher.kt
@@ -3,14 +3,12 @@ class Cipher(val _key:String? = null) {
     private val source="abcdefghijklmnopqrstuvwxyz".toCharArray()
     val key:String
     init { 
-        
         if(_key == null) {
             key = Random().ints(100, 0, 26).toArray().map() { i -> source[i] }.joinToString("")
         } else if (_key.length == 0) {
             throw IllegalArgumentException()
         } else if (_key.toCharArray().asList().filter() { i -> source.indexOf(i) == -1 }.size > 0) {
             throw IllegalArgumentException()
-          
         } else {
             key = _key
         }
@@ -21,5 +19,4 @@ class Cipher(val _key:String? = null) {
     fun decode(s: String) : String {
         return s.toCharArray().mapIndexed() { i, c -> source[(source.indexOf(c) - source.indexOf(key.toCharArray()[i]) + source.size) % source.size]}.joinToString("")
     }
-
 }
