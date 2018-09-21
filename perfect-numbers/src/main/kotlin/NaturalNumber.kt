07811e2bafd9623fdbad62f0fc3d00a6c4d8bf82
Ray Deck <ray@raydeck.com>
2018-09-21 10:30:17 -0400

    Adding perfect numbers

--- perfect-numbers/src/main/kotlin/NaturalNumber.kt
+++ perfect-numbers/src/main/kotlin/NaturalNumber.kt
@@ -4,12 +4,11 @@ enum class Classification {
 }
 
 fun classify(naturalNumber: Int): Classification {
-    if (naturalNumber < 1) throw RuntimeException() 
-    var factors = (1..naturalNumber/2).fold(mutableListOf<Int>()) { factors, factor -> 
-        if (naturalNumber.rem(factor) == 0) factors.add(factor)
-        factors
+    if (naturalNumber < 1) throw RuntimeException("Natural numbers are positive integers") 
+    val factors = (1..naturalNumber/2).fold(listOf<Int>()) { factors, factor -> 
+        if (naturalNumber.rem(factor) == 0) factors.plusElement(factor) else factors
     }
-    val sumOfFactors = factors.fold(0) { factor, product -> product + factor }
+    val sumOfFactors = factors.fold(0) { factor, total -> total + factor }
     return when {
         (naturalNumber == sumOfFactors) -> Classification.PERFECT
         (naturalNumber < sumOfFactors ) -> Classification.ABUNDANT
