5bbdb69e735137b57d37ada8c089a1189d42d6fd
Ray Deck <ray@raydeck.com>
2018-09-27 15:01:43 -0400

    Terser answer to flatten-array

--- flatten-array/bin/flatterner.kt
+++ flatten-array/bin/flatterner.kt
@@ -1,12 +1,6 @@
 package Flattener
 fun flatten(list:List<Any?>): List<Int> = list.fold(listOf<Int>()) { a, e -> when (e) {
     is Int -> a.plus(e)
-    is List<*> -> {
-        val t = e as? List<Any>
-        when { 
-           t == null -> a
-           else -> a.plus(flatten(e)) 
-        }
-    }
+    is List<*> -> if(e is List<*>) a.plus(flatten(e)) else a 
     else -> a
 }}
