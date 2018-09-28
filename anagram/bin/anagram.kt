d8c6d1125bb60ec04a2905282d1c3103aa348256
Ray Deck <ray@raydeck.com>
2018-09-28 15:54:07 -0400

    Spacing

--- anagram/bin/anagram.kt
+++ anagram/bin/anagram.kt
@@ -1,7 +1,7 @@
 class Anagram(val seed:String) {
     val lcase = seed.toLowerCase()
     val sorted = lcase.toCharArray().sorted()
-    fun match(l:List<String>): Set<String> =  l.filter() { s -> 
+    fun match(l:List<String>): Set<String> = l.filter() { s -> 
         val l = s.toLowerCase()
         when {
             l == lcase -> false
