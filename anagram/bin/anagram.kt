693949f6e4dd2ea1fc87234b1bfcb39bfb9ec95e
Ray Deck <ray@raydeck.com>
2018-09-28 15:50:54 -0400

    Removing unnecessary return statement

--- anagram/bin/anagram.kt
+++ anagram/bin/anagram.kt
@@ -1,11 +1,11 @@
 class Anagram(val seed:String) {
     val lcase = seed.toLowerCase()
     val sorted = lcase.toCharArray().sorted()
-    fun match(l:List<String>): Set<String> { return l.filter() { s -> 
+    fun match(l:List<String>): Set<String> =  l.filter() { s -> 
         val l = s.toLowerCase()
         when {
             l == lcase -> false
             else -> l.toCharArray().sorted() == sorted 
         }
-    }.toSet() }
+    }.toSet() 
 }
