317e337b2dfc8169ed03c4af7226a5ff9d6b7a99
Ray Deck <ray@raydeck.com>
2018-09-28 09:59:27 -0400

    Oof fixing piglatin typo

--- pig-latin/bin/piglatin.kt
+++ pig-latin/bin/piglatin.kt
@@ -13,7 +13,8 @@ fun translate(source: String): String = source.split(" ").map() {word ->
                             when (word.get(vowelIndex -1)) {
                                 'q' ->  word.toLowerCase().substring(vowelIndex + 1) + word.toLowerCase().substring(0, vowelIndex + 1) + "ay"
                                 else -> word.toLowerCase().substring(vowelIndex) + word.toLowerCase().substring(0, vowelIndex) + "ay"
-                            }                   
+                            }   
+                        }                
                         else -> word.toLowerCase().substring(vowelIndex) + word.toLowerCase().substring(0, vowelIndex) + "ay"
                     }
                     when (word.get(0) in ('A'..'Z')) {
