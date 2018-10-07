d73f466f1417efe418fb597dd5b0a9cb94c37661
Ray Deck <ray@raydeck.com>
2018-10-07 14:45:08 -0400

    Fixing mod/base switch in roman numerals

--- roman-numerals/src/main/kotlin/avecaesar.kt
+++ roman-numerals/src/main/kotlin/avecaesar.kt
@@ -1,12 +1,12 @@
 package RomanNumeral
-private fun valString(mod: Int, one: Char, five: Char = '?', ten: Char = '?'): String = when (mod) {
+private fun valString(base: Int, one: Char, five: Char = '?', ten: Char = '?'): String = when (base) {
     9 -> "" + one + ten
-    in (6..8) -> five + (6..mod).map(){one}.joinToString("")
+    in (6..8) -> five + (6..base).map(){one}.joinToString("")
     5 -> "" + five
     4 -> "" + one + five
-    in (1..3) -> (1..mod).map(){one}.joinToString("")
+    in (1..3) -> (1..base).map(){one}.joinToString("")
     0 -> ""
-    else -> throw IllegalStateException("Math is broken ${mod}")
+    else -> throw IllegalStateException("Math is broken ${base}")
 }
 fun value(input: Int): String = valString(input / 1000, 'M') + 
     valString((input % 1000) / 100, 'C', 'D', 'M') + 
